46ac049e8a268963a175bfbdcf9fa21b382ed705
Przemyslaw Juda <przemyslaw.juda@gmail.com>
2021-10-20 23:10:54 +0200

    Update broken channel example

--- examples/broken_channel/run-popex.py
+++ examples/broken_channel/run-popex.py
@@ -9,17 +9,17 @@ import forward
 
 def main():
     deesse_simulator = geostat.DeesseSimulator()
-    flow_solver = forward.FlowSolver(path_results = 'modflow-10000')
+    flow_solver = forward.FlowSolver(path_results = 'modflow')
     
     problem = Problem(generate_m=deesse_simulator.generate_m, # model generation function
                       compute_log_p_lik=flow_solver.compute_log_p_lik, # log likelihood (forward)
                       get_hd_pri=deesse_simulator.get_hd_pri) # prior hard conditioning
 
     algorithm.run_popex_mp(pb=problem,
-                           path_res='popex-10000/',
+                           path_res='popex',
                            path_q_cat=None,
                            nmp=40,
-                           nmax=10000,
+                           nmax=1000,
                            ncmax=(10,),
                            n_prior=1000)
 
